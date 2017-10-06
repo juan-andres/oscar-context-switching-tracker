f3d8f2a6a125cff5b1529d2b799b32755256e8ac
juan andres andrango <andrango.juanandres@gmail.com>
2017-10-06 01:31:14 -0400

    Minor refactor
    Bug fix: Clicking on new activity when current activity is interrupted does not hide the continue bin

--- app.js
+++ app.js
@@ -2,96 +2,88 @@
   TODOs
   - Update tab every minute
   - Record how many interrupts and how long each (DONE)
+  - Disable `start new activity` btn if no text is present
 **/
 
 const STATUS = {
   RECORDING: 1,
-  PAUSED: 2,
+  INTERRUPTED: 2,
   FINISHED: 3,
-  IDLING: 4, // can't pause/unpaused
 };
 
 const $current_activity = document.getElementById('current_activity');
 const $activity_input = document.getElementById('activity_input');
+const $activity_log = document.getElementsByClassName('activity_log')[0];
+
 const $start_btn = document.getElementById('start_btn');
 const $finish_btn = document.getElementById('finish_btn');
 const $interrupt_btn = document.getElementById('interrupt_btn');
 const $continue_btn = document.getElementById('continue_btn');
-const $activity_log = document.getElementsByClassName('activity_log')[0];
-const $debug = document.getElementsByClassName('debug')[0];
+
+function prettyTime(ms) {
+  const mins = ((ms / 60000) | 0) % 60;
+  const seconds = ((ms / 1000) | 0) % 60;
+  if (mins > 0) {
+    const hours = (ms / 3600000) | 0;
+    return hours !== 0 ? `${hours}h ${mins}m` : `${mins}m ${seconds}s`;
+  } else {
+    return `${seconds}s`;
+  }
+}
 
 class Activity {
-  constructor(name, status) {
+  constructor(name) {
     this.name = name;
-    this.status = status;
+    this.status = STATUS.PAUSED;
     this.startTimeStamp = Date.now();
-    this.finishTimeStamp = -1;
+    this.finishTimeStamp = null;
     this.interruptions = []; // Activities
   }
 
   pause() {
-    if (this.status === STATUS.IDLING || this.status === STATUS.PAUSED) return;
+    if (this.status === STATUS.PAUSED) return;
     this.status = STATUS.PAUSED;
     // TODO allow user to input the name of the interruption
-    this.interruptions.push(new Activity('interruption:custom name'));
+    const interruption = new Activity('interruption:custom name');
+    interruption.record();
+    this.interruptions.push(interruption);
   }
 
-  unpause() {
-    if (this.status === STATUS.IDLING || this.status !== STATUS.PAUSED) return;
+  record() {
+    if (this.status === STATUS.RECORDING) return;
     this.status = STATUS.RECORDING;
+    this._finishLastInterruption()
+  }
+
+  finish() {
+    if (this.status === STATUS.FINISH) return;
+    this.status = STATUS.FINISHED;
+    this._finishLastInterruption();
+    this.finishTimeStamp = Date.now();
+    return this;
+  }
+
+  _finishLastInterruption() {
     if (this.interruptions.length > 0) {
       const lastInterruption = this.interruptions[this.interruptions.length - 1];
       lastInterruption.finish();
     }
   }
 
-  finish() {
-    this.unpause();
-    this.status = STATUS.FINISHED;
-    this.finishTimeStamp = Date.now();
-    return this;
-  }
-
   toString() {
-    return `${this.name} ${this.prettyDuration()}`;
-  }
-
-  static prettyTime(ms) {
-    // DEBUGGING
-    // const seconds = (ms / 1000) | 0;
-    // return `${seconds}s`;
-
-    const mins = ((ms / 60000) | 0) % 60;
-    const seconds = ((ms / 1000) | 0) % 60;
-    if (mins !== 0) {
-      const hours = (ms / 3600000) | 0;
-      return hours !== 0 ? `${hours}h ${mins}m` : `${mins}m ${seconds}s`;
-    } else {
-      return `${seconds}s`;
-    }
-  }
-
-  prettyDuration() {
-    const activityMs = this.duration();
-
-    if (this.status === STATUS.IDLING) {
-      return `${Activity.prettyTime(activityMs)} idling...`;
-    }
-
+    const activityMs = this.elapsedTime();
+    const interruptionsMs = this.interruptionsTime();
     const nInterruptions = this.interruptions.length;
-    const totalInterruptedMs = this.interruptionsDuration();
-
-    return `${Activity.prettyTime(activityMs)} (interrupted ${nInterruptions} times ${Activity.prettyTime(totalInterruptedMs)} )`;
+    return `${this.name} ${prettyTime(activityMs - interruptionsMs)} (interrupted ${nInterruptions} times ${prettyTime(interruptionsMs)} )`;
   }
 
-  duration() {
+  elapsedTime() {
     const finishTimeStamp = this.status === STATUS.FINISHED ? this.finishTimeStamp : Date.now();
-    const totalElapsedTime = finishTimeStamp - this.startTimeStamp;
-    return totalElapsedTime - this.interruptionsDuration();
+    return finishTimeStamp - this.startTimeStamp;
   }
 
-  interruptionsDuration() {
-    return this.interruptions.reduce((sum, i) => {return sum + i.duration()}, 0);
+  interruptionsTime() {
+    return this.interruptions.reduce((sum, i) => {return sum + i.elapsedTime()}, 0);
   }
 }
 
@@ -104,51 +96,54 @@ class App {
     $start_btn.addEventListener('click', this.startNewActivity.bind(this));
     $finish_btn.addEventListener('click', this.finishCurrentActivity.bind(this));
     $interrupt_btn.addEventListener('click', this.pauseCurrentActivity.bind(this));
-    $continue_btn.addEventListener('click', this.unpauseCurrentActivity.bind(this));
+    $continue_btn.addEventListener('click', this.continueCurrentActivity.bind(this));
   }
 
   logCurrentActivity() {
-    if (this.currentActivity && this.currentActivity.status !== STATUS.FINISHED) {
-      const $activity = document.createElement('li');
-      $activity.innerHTML = this.currentActivity.finish().toString();;
-      $activity_log.insertBefore($activity, $activity_log.firstChild);
-    }
+    this.history.push(this.currentActivity);
+    const $activity = document.createElement('li');
+    $activity.innerHTML = this.currentActivity.toString();
+    $activity_log.insertBefore($activity, $activity_log.firstChild);
   }
 
   startNewActivity() {
-    // TODO (juanandres) disable if not input
-    this.logCurrentActivity();
     if (this.currentActivity) {
-      this.history.push(this.currentActivity);
+      this.currentActivity.finish();
+      this.logCurrentActivity();
     }
-    this.currentActivity = new Activity($activity_input.value, STATUS.RECORDING);
+    this.currentActivity = new Activity($activity_input.value);
+    this.currentActivity.record();
+
     $activity_input.value = null;
     $finish_btn.classList.remove("hidden");
     $interrupt_btn.classList.remove("hidden");
+    $continue_btn.classList.add("hidden");
   }
 
   finishCurrentActivity() {
-    this.logCurrentActivity();
-    this.unpauseCurrentActivity();
-    $finish_btn.classList.add("hidden");
-    this.currentActivity.finish();
+    if (this.currentActivity) {
+      this.currentActivity.finish();
+      this.logCurrentActivity();
+    }
 
-    this.history.push(this.currentActivity);
-    this.currentActivity = new Activity('Waiting for work ...', STATUS.IDLING);
+    $finish_btn.classList.add("hidden");
     $interrupt_btn.classList.add("hidden");
+    $continue_btn.classList.add("hidden");
+
+    this.currentActivity = null;
   }
 
   pauseCurrentActivity() {
-    if (!this.currentActivity || this.currentActivity.status === STATUS.PAUSED) return;
-    $interrupt_btn.classList.toggle("hidden");
-    $continue_btn.classList.toggle("hidden");
+    if (!this.currentActivity) return;
     this.currentActivity.pause();
+    $interrupt_btn.classList.add("hidden");
+    $continue_btn.classList.remove("hidden");
   }
-  unpauseCurrentActivity() {
-    if (!this.currentActivity || this.currentActivity.status !== STATUS.PAUSED) return;
-    $interrupt_btn.classList.toggle("hidden");
-    $continue_btn.classList.toggle("hidden");
-    this.currentActivity.unpause();
+  continueCurrentActivity() {
+    if (!this.currentActivity) return;
+    this.currentActivity.record();
+    $interrupt_btn.classList.remove("hidden");
+    $continue_btn.classList.add("hidden");
   }
 
   // TODO (juanandres) not proud of this of course!
